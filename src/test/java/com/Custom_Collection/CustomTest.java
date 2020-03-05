bd455f765c5ca5d719bdf6b7a351e36e43729518
kalyan kamati <48156771+kalyankamati@users.noreply.github.com>
2020-03-05 19:40:53 +0530

    test

---
+++ src/test/java/com/Custom_Collection/CustomTest.java
@@ -0,0 +1,14 @@
+package com.Custom_Collection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
